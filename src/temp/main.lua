dbffe38cd51939e5c7dea5a9510ac7be910577fa
Marek Słomnicki <marek@slomnicki.net>
2017-03-16 11:03:19 +0100

    MQTT script

--- src/temp/main.lua
+++ src/temp/main.lua
@@ -1,12 +1,4 @@
--- Timers
--- 0 = WiFi status + mqtt connect
--- 1 = MQTT offline
--- 2 = Free
--- 3 = Free
--- 4 = Free
--- 5 = MQTT activity
--- 6 = Button debounce
- 
+git clo
 mqttBroker = "192.168.1.30"
 mqttUser = "none"
 mqttPass = "none"
@@ -14,33 +6,8 @@ mqttPass = "none"
 deviceID="bugzapper"
 roomID="1"
  
-wifi.setmode(wifi.STATION)
-wifi.sta.config ("SSID", "PASSKEY")
- 
- 
--- Pin which the relay is connected to
-relayPin = 6
-gpio.mode(relayPin, gpio.OUTPUT)
-gpio.write(relayPin, gpio.LOW)
- 
- 
--- Connected to switch with internal pullup enabled
-buttonPin = 3
-buttonDebounce = 250
-gpio.mode(buttonPin, gpio.INPUT, gpio.PULLUP)
- 
- 
--- MQTT led
-mqttLed=7
-gpio.mode(mqttLed, gpio.OUTPUT)
-gpio.write(mqttLed, gpio.HIGH)
- 
 -- Make a short flash with the led on MQTT activity
-function mqttAct()
-    if (gpio.read(mqttLed) == 1) then gpio.write(mqttLed, gpio.HIGH) end
-    gpio.write(mqttLed, gpio.LOW)
-    tmr.alarm(5, 50, 0, function() gpio.write(mqttLed, gpio.HIGH) end)
-end
+
  
 m = mqtt.Client("Sonoff-" .. deviceID, 180, mqttUser, mqttPass)
 m:lwt("/lwt", "Sonoff " .. deviceID, 0, 0)
@@ -53,27 +20,7 @@ m:on("offline", function(con)
 end)
  
  
--- Pin to toggle the status
-buttondebounced = 0
-gpio.trig(buttonPin, "down",function (level)
-    if (buttondebounced == 0) then
-        buttondebounced = 1
-        tmr.alarm(6, buttonDebounce, 0, function() buttondebounced = 0; end)
-      
-        --Change the state
-        if (gpio.read(relayPin) == 1) then
-            gpio.write(relayPin, gpio.LOW)
-            print("Was on, turning off")
-        else
-            gpio.write(relayPin, gpio.HIGH)
-            print("Was off, turning on")
-        end
-         
-        mqttAct()
-        mqtt_update()
-    end
-end)
- 
+
  
 -- Update status to MQTT
 function mqtt_update()
@@ -113,7 +60,7 @@ tmr.alarm(0, 1000, 1, function()
     if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then  
         tmr.stop(0)
         m:connect(mqttBroker, 1883, 0, function(conn)
-            gpio.write(mqttLed, gpio.HIGH)
+            gpio.write(ledPin, gpio.HIGH)
             print("MQTT connected to:" .. mqttBroker)
             mqtt_sub() -- run the subscription function
         end)
