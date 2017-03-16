def9964ef632b7d716388be3416d2effad20c4ab
Marek Słomnicki <marek@slomnicki.net>
2017-03-16 09:34:42 +0100

    cleanup

--- src/config-esp-01.lua
+++
@@ -1,8 +0,0 @@
-password="changemeNOW"
-
-service_button=3 --GPIO0a- pin wybierajacy flashowanie lub normalne uruchomienie
-
--- Jeżeli ma być współdzielony z resetem, musi to być inny niz GPIO0 lub GPIO2
-service_led=4 --GPIO2 dioda serwisowa - niebieska dioda na ESP12
-
-
